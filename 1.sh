5e2d3fe3939a91eb61941762f76bf81aeda57547
batonser <batonser@users.noreply.github.com>
2022-08-31 20:19:18 +0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,3 +2,4 @@
 
 cat /root/file.txt | while read f; do rsync -avze 'ssh -p 53120' root@46.61.214.94:"$f" /tmp/test/ ; done
 cat /root/url.txt | while read url; do cd /tmp/test && wget "$url" ; done 
+ 
