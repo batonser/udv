bd53372afcd583e2ae6a6635af65ec40b74ca217
batonser <batonser@users.noreply.github.com>
2022-09-04 17:12:52 +0500

    Delete 1.sh

--- 1.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-cat /root/file.txt | while read f; do rsync -avze 'ssh -p 53120' root@46.61.214.94:"$f" /tmp/test/ ; done
-cat /root/url.txt | while read url; do cd /tmp/test && wget "$url" ; done 
- 
